f389939536e72a67a4edfe50f9e40d2fe58b5888
laireyx <laireyx@gmail.com>
2024-02-29 01:03:07 +0900

    feat: add disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Suspense } from 'react';
 import './App.css';
 import LoadingFallback from './components/LoadingFallback';
 import Controller from '@components/Controller';
+import Disclaimer from '@components/Disclaimer';
 
 function App() {
   return (
     <Suspense fallback={<LoadingFallback />}>
       <Controller />
+      <Disclaimer />
     </Suspense>
   );
 }
---
+++ src/components/Disclaimer/index.tsx
@@ -0,0 +1,14 @@
+export default function Disclaimer() {
+  return (
+    <p>
+      유의사항
+      <br />
+      <ul>
+        <li>
+          보스전에 입장하지 않았을 경우 버프 표시 영역이 달라져 인식 결과에
+          문제가 발생할 수 있습니다.
+        </li>
+      </ul>
+    </p>
+  );
+}
